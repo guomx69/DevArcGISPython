ecaf49aa5414288f0695fce4ecce9b1f4b65e2f2
ming <mint@hp2014>
2024-04-24 12:12:25 -0500

    devop temporary saving

--- startArcGisInConda.sh
+++ startArcGisInConda.sh
@@ -1,3 +1,15 @@
  #!/bin/bash
  #conda info  --envs
- conda activate ArcgisPython3.11
+
+__conda_setup="$('/home/mint/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/mint/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/mint/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/mint/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+conda activate ArcgisPython3.11
